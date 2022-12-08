9f3fcc53632081a345751c211d33a410dcc97897
AbderrahimAgra <93934437+AbderrahimAgra@users.noreply.github.com>
2022-12-08 16:51:21 +0100

    Mar 9 crud annonces de repas categories (#15)
    * MAR-43 jai ajouter model category repas ordertime
    
    * MAR-43 jai ajouter model category repas ordertime
    
    * resolve probleme
    
    * MAR-22 jai fini mon tache partie  crud de category
    
    Co-authored-by: elmehdinainia <mehdinainiaa4@gmail.com>

--- backend/models/index.js
+++ backend/models/index.js
@@ -12,9 +12,15 @@ db.order = require('./orderModel')
 db.status = require('./statusModel')
 
 
+
+
+
 db.meal = require('./meal')
 db.category = require('./category')
 db.ordertime = require('./ordertime')
+
+
+
 
 
 db.role.estimatedDocumentCount((err, count) => {
