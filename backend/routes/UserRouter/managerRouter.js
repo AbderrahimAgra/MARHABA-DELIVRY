0e010e59545a62a585bb74f849e2a0402fe0be3c
elmehdinainia <mehdinainiaa4@gmail.com>
2022-12-09 16:39:42 +0100

    MAR-21 jai regler un probleme au niveau f

--- backend/routes/UserRouter/managerRouter.js
+++ backend/routes/UserRouter/managerRouter.js
@@ -5,7 +5,9 @@ const {
   findcategory,
   deletcategory,
   updatecategory,
-  updateuser
+  updateuser,
+  listclient,
+  listlivreur
 } = require('../../controllers/userController/managerController')
 
 // Error Handler
@@ -18,6 +20,8 @@ router.get('/manager/findcategory', tryCatch(findcategory))
 router.delete('/manager/deletcategory/:id', tryCatch(deletcategory))
 router.put('/manager/updatecategory/:id', tryCatch(updatecategory))
 router.put('/manager/updateuser/:id', tryCatch(updateuser))
+router.get('/manager/listclient', tryCatch(listclient))
+router.get('/manager/listlivreur', tryCatch(listlivreur))
 
 
 router.use(errorHandler)
