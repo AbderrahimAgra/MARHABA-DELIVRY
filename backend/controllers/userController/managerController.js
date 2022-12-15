2c0a2b586a4d8bbf089113bd3f8bf2050bd1d7c5
abdenacer <nasseressaouira@gmail.com>
2022-12-15 14:08:49 +0800

    fini les taches cote front-end

--- backend/controllers/userController/managerController.js
+++ backend/controllers/userController/managerController.js
@@ -11,7 +11,7 @@ const managerUser = async (req, res) => {
   const token_user = await jwt.verify(token, process.env.SECRET)
   const user = await User.findById(token_user.id)
   const role_user = await Role.findById(user.role)
-  if(role_user.name != "manager") throw Error("You Can' To Access in This Page")
+  if (role_user.name != "manager") throw Error("You Can' To Access in This Page")
   res.json({
     first_name: user.first_name,
     last_name: user.last_name,
@@ -21,99 +21,99 @@ const managerUser = async (req, res) => {
 }
 
 const addcategory = async (req, res) => {
-    const {name} = req.body
-    if(name == '') throw Error("Please Fill The name")
-    const category = await Category.findOne({name})
-    if(category) throw Error("the category deja already")
-    if(!category){
-        const category_create = await Category.create({name})
-        if(category_create){
-          res.json({ 
-            name: name,
-            message: "created category"
-          })
-        }
-
-   //!------------------------------------------------autre methode create---------------------------------
-      // const created_category = new Category({
-      //   name:name
-      // })
-      // await created_category.save()
-      //  res.json({
-      //    mesage:" category created"
-      //  })
-  
+  const { name } = req.body
+  if (name == '') throw Error("Please Fill The name")
+  const category = await Category.findOne({ name })
+  if (category) throw Error("the category deja already")
+  if (!category) {
+    const category_create = await Category.create({ name })
+    if (category_create) {
+      res.json({
+        name: name,
+        message: "created category"
+      })
     }
+
+    //!------------------------------------------------autre methode create---------------------------------
+    // const created_category = new Category({
+    //   name:name
+    // })
+    // await created_category.save()
+    //  res.json({
+    //    mesage:" category created"
+    //  })
+
+  }
 
 }
 
 const findcategory = async (req, res) => {
-   const findcategoris = await Category.findOne()
-   if(findcategoris){
+  const findcategoris = await Category.findOne()
+  if (findcategoris) {
     res.json(findcategoris)
-   }
+  }
 }
 
 const deletcategory = async (req, res) => {
-  const {id} = req.params.id
-  const finddeleted = await Category.findOne({id})
-  if(finddeleted){ 
+  const { id } = req.params.id
+  const finddeleted = await Category.findOne({ id })
+  if (finddeleted) {
     await finddeleted.remove()
     res.send(`${finddeleted.name} deleted succesfully`)
   }
   else throw Error('not deleted')
 
 }
+
 const updatecategory = async (req, res) => {
-  const {id} = req.params
-  
-    const data =  await Category.findOneAndUpdate({_id:id},{name: req.body.name})
-    if(!data) res.send('not')
-    res.send('updated')
+  const { id } = req.params
+
+  const data = await Category.findOneAndUpdate({ _id: id }, { name: req.body.name })
+  if (!data) res.send('not')
+  res.send('updated')
 
 }
+
 const updateuser = async (req, res) => {
-  const {id} = req.params
+  const { id } = req.params
 
+  const data = await User.findOne({ _id: id })
+  if (data.isBanned == true) {
+    await User.findOneAndUpdate({ _id: id }, { isBanned: false })
+  } else {
+    await User.findOneAndUpdate({ _id: id }, { isBanned: true })
+  }
+  res.send(data)
 
-    const data =  await User.findOne({_id:id})
-    if(data.isBanned==true){
-      await User.findOneAndUpdate({_id:id},{isBanned: false})
-    }else{
-       await User.findOneAndUpdate({_id:id},{isBanned: true})
-    }
-    res.send(data)
-    
 
 }
 
 const listclient = async (req, res) => {
+  const id_role = '638f45b410a60d0c0019353a'
 
-    const id_role = '638f45b410a60d0c0019353a'
-    const findclient = await User.find({role: id_role})
-    if(findclient){
-      res.send(
-     findclient
-      )
-    }
-    else{
-      throw Error ('Not User to role client')
-    }
+  const findclient = await User.find({ role: id_role })
+  if (findclient) {
+    res.send(
+      findclient
+    )
+  }
+  else {
+    throw Error('Not User to role client')
+  }
 }
 
 const listlivreur = async (req, res) => {
-
   const id_role = '638f45b410a60d0c0019353b'
-  const findclient = await User.find({role: id_role})
-  if(findclient){
+
+  const findclient = await User.find({ role: id_role })
+  if (findclient) {
     res.send(
-   findclient
+      findclient
     )
   }
-  else{
-    throw Error ('Not User to role client')
+  else {
+    throw Error('Not User to role client')
   }
-
 }
 
 
---
+++ frontend-vite/src/App.jsx
@@ -0,0 +1,52 @@
+import { React } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css';
+// Pages the Auth
+import Login from './pages/auth/Login'
+import Signup from './pages/auth/Signup'
+import ForgotPassword from './pages/auth/ForgotPassword'
+import FormForgotPassword from './pages/auth/FormForgotPassword'
+// Pages Client
+import Dashboard from './components/layouts/Dashboard';
+import DashbordClient from './pages/user/client/dashboardClient'
+import SettingClient from './pages/user/client/settingClient'
+// Pages Manager
+import DashbordManager from './pages/user/manager/dashboardManager'
+import RepasManager from './pages/user/manager/repasManager'
+import CommandManager from './pages/user/manager/commandManager'
+import CategoryManager from './pages/user/manager/categoryManager'
+import LivreursManager from './pages/user/manager/LivreursManager'
+import ClientsManager from './pages/user/manager/clientsManager'
+import SettingManager from './pages/user/manager/settingManager'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* { Auth} */}
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/form-forgot-password/' element={<FormForgotPassword />} />
+        {/* { Client } */}
+        <Route path='/dashboard/client' element={<Dashboard />}>
+          <Route path='' element={<DashbordClient />} />
+          <Route path='setting' element={<SettingClient />} />
+        </Route>
+        {/* { Manager } */}
+        <Route path='/dashboard/manager' element={<Dashboard />}>
+          <Route path='' element={<DashbordManager/>} />
+          <Route path='repas' element={<RepasManager />} />
+          <Route path='category' element={<CategoryManager />} />
+          <Route path='command' element={<CommandManager />} />
+          <Route path='livreurs' element={<LivreursManager />} />
+          <Route path='clients' element={<ClientsManager />} />
+          <Route path='setting' element={<SettingManager />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
