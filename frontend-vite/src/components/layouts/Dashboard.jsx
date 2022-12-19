0a20c0d6de5fa909e30a57e4c4eecf7bf4155cab
abdenacer <nasseressaouira@gmail.com>
2022-12-19 16:44:49 +0800

    modifier sidebar par le role

--- frontend-vite/src/components/layouts/Dashboard.jsx
+++ frontend-vite/src/components/layouts/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link, Outlet } from 'react-router-dom';
 import { AiOutlineDashboard, AiFillSetting, AiOutlineLogout } from 'react-icons/ai';
-import { GiMeal } from 'react-icons/gi';
+import { GiMeal, GiHotMeal } from 'react-icons/gi';
 import { BiCategoryAlt, BiCommand, BiAddToQueue } from 'react-icons/bi';
 import { BsFillCartCheckFill } from 'react-icons/bs';
 import { FiUsers } from 'react-icons/fi';
@@ -27,16 +27,19 @@ function logout() {
 const Dashboard = () => {
   const [open, setOpen] = useState(true)
 
-  // const MenusClient = [
-  //   { title: "Dashboad", icon: AiOutlineDashboard, route: '' },
-  //   { title: "Salade", icon: GiHotMeal, gap: true, route: '/' },
-  //   { title: "Pizza", icon: GiHotMeal, route: '/' },
-  //   { title: "Tacos", icon: GiHotMeal, route: '/' },
-  //   { title: "Sandwich", icon: GiHotMeal, route: '/' },
-  //   { title: "Desserts", icon: GiHotMeal, route: '/' },
-  //   { title: "Setting", icon: AiFillSetting, gap: true, route: 'setting' },
-  //   { title: "Logout", icon: AiOutlineLogout, route: '/login' },
-  // ]
+  const role = localStorage.getItem('role')
+
+
+  const MenusClient = [
+    { title: "Dashboad", icon: AiOutlineDashboard, route: '' },
+    { title: "Salade", icon: GiHotMeal, gap: true, route: '/' },
+    { title: "Pizza", icon: GiHotMeal, route: '/' },
+    { title: "Tacos", icon: GiHotMeal, route: '/' },
+    { title: "Sandwich", icon: GiHotMeal, route: '/' },
+    { title: "Desserts", icon: GiHotMeal, route: '/' },
+    { title: "Setting", icon: AiFillSetting, gap: true, route: 'setting' },
+    { title: "Logout", icon: AiOutlineLogout, route: '/login' },
+  ]
 
   const MenusManager = [
     { title: "Dashboad", icon: AiOutlineDashboard, route: '' },
@@ -63,26 +66,29 @@ const Dashboard = () => {
               className={`cursor-pointer w-16`}
             />
           </div>
-          {/* Menus Client */}
-          {/* <ul className="pt-6">
-            {MenusClient.map((menu, index) => (
-              <li key={index} className={`text-gray-300 text-sm flex w-11 items-center gap-x-4 cursor-pointer p-2 hover:bg-zinc-800 rounded-md ${menu.gap ? "mt-12" : " "}`}>
-                <div className="text-white">{React.createElement(menu?.icon, { size: "25" })}</div>
-                <Link to={menu.route} onClick={logout}><span className={`${!open && 'hidden'} origin-left duration-200 text-lg text-white`}>{menu.title}</span></Link>
-              </li>
-            ))}
-          </ul> */}
+          {role === 'manager' ? (
+            <ul className="pt-6">
+              {MenusManager.map((menu, index) => (
+                <li key={index} className={`text-gray-300 text-sm flex w-11 items-center gap-x-4 cursor-pointer p-2 hover:bg-zinc-800 rounded-md ${menu.gap ? "mt-12" : " "}`}>
+                  <div className="text-white">{React.createElement(menu?.icon, { size: "25" })}</div>
+                  <Link to={menu.route} onClick={logout}><span className={`${!open && 'hidden'} origin-left duration-200 text-lg text-white`}>{menu.title}</span></Link>
+                </li>
+              ))}
+            </ul>)
+            :
+            role === 'client' ? (
+              <ul className="pt-6">
+                {MenusClient.map((menu, index) => (
+                  <li key={index} className={`text-gray-300 text-sm flex w-11 items-center gap-x-4 cursor-pointer p-2 hover:bg-zinc-800 rounded-md ${menu.gap ? "mt-12" : " "}`}>
+                    <div className="text-white">{React.createElement(menu?.icon, { size: "25" })}</div>
+                    <Link to={menu.route} onClick={logout}><span className={`${!open && 'hidden'} origin-left duration-200 text-lg text-white`}>{menu.title}</span></Link>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
 
-          {/* Menus Manager */}
-          <ul className="pt-6">
-            {MenusManager.map((menu, index) => (
-              <li key={index} className={`text-gray-300 text-sm flex w-11 items-center gap-x-4 cursor-pointer p-2 hover:bg-zinc-800 rounded-md ${menu.gap ? "mt-12" : " "}`}>
-                <div className="text-white">{React.createElement(menu?.icon, { size: "25" })}</div>
-                <Link to={menu.route} onClick={logout}><span className={`${!open && 'hidden'} origin-left duration-200 text-lg text-white`}>{menu.title}</span></Link>
-              </li>
-            ))}
-          </ul>
         </div>
+
         <div className="p-3 px-5 text-2xl font-semibold flex-1 h-screen">
           <nav className={`${open ? 'ml-72' : ''} bg-black ml-20 text-white border-gray-200 px-2 rounded-xl sm:px-4 py-2.5 dark:bg-gray-900`}>
 
@@ -91,9 +97,12 @@ const Dashboard = () => {
                 <img src="../../../public/assets/logo.png" className="h-6 mr-3 sm:h-9" alt="Marhaba Logo" />
               </a>
               <div class="flex items-center md:order-2">
+                { role === 'client' ?
                 <div>
                   <BsFillCartCheckFill className='mr-2' />
                 </div>
+                : null
+                }
                 <button type="button" class="flex mr-3 text-sm  rounded-full md:mr-0" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                   <img className="w-10 h-10 rounded-full bg-white" src="../../../public/assets/profil.png" alt="pPofil photo" />
                 </button>
@@ -103,9 +112,10 @@ const Dashboard = () => {
           {<Outlet />}
 
         </div>
+
       </div>
 
-    </div>
+    </div >
   )
 }
 
