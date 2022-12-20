63a7f07346408378a0fd07d6ed49a7b715e17318
abdenacer <nasseressaouira@gmail.com>
2022-12-20 13:54:10 +0800

    modifier sur sidebar des projet en generale

--- frontend-vite/src/pages/user/client/dashboardClient.jsx
+++ frontend-vite/src/pages/user/client/dashboardClient.jsx
@@ -1,79 +1,72 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
+import { AiOutlineDashboard, AiFillSetting, AiOutlineLogout } from 'react-icons/ai';
+import { GiHotMeal } from 'react-icons/gi';
+import axios from "axios";
+
+const baseURL = 'http://localhost:5500/api/auth'
 
 
 function Client() {
+
+  function logout() {
+    axios.get(`${baseURL}/logout`)
+      .then((res) => {
+        if (res.data) {
+          localStorage.clear()
+        }
+      })
+      .catch(err =>
+        console.log(err)
+      )
+  }
+
+  const [open, setOpen] = useState(true)
+
+  const MenusClient = [
+    { title: "Dashboad", icon: <AiOutlineDashboard />, route: '/dashboard/client' },
+    { title: "Salade", icon: <GiHotMeal />, gap: true, route: '/' },
+    { title: "Pizza", icon: <GiHotMeal />, route: '/' },
+    { title: "Tacos", icon: <GiHotMeal />, route: '/' },
+    { title: "Sandwich", icon: <GiHotMeal />, route: '/' },
+    { title: "Desserts", icon: <GiHotMeal />, route: '/' },
+    { title: "Setting", icon: <AiFillSetting />, gap: true, route: '/dashboard/client/setting' },
+    { title: "Logout", icon: <AiOutlineLogout />, route: '/login' },
+  ]
+
   return (
     <div>
+      <div className={`${open ? 'w-72' : 'w-20'} duration-300 fixed top-0 px-5 min-h-screen  bg-dark`}>
+        <img src="../../../../public/assets/left-arrow.png" className={`bg-white absolute cursor-pointer rounded-full -right-3 top-9 w-8 border-4 p-1 border-dark ${!open && "rotate-180"}`} onClick={() => setOpen(!open)} />
+        <div className="flex gap-x-4 items-center justify-center ">
+          <img src="../../../../public/assets/logo.png" className={`cursor-pointer w-16`} />
+        </div>
+        <ul className="pt-6">
+          {MenusClient.map((menu, index) => (
+            <li key={index} className={`text-gray-300 text-sm flex w-11 items-center gap-x-4 cursor-pointer p-2 hover:bg-zinc-800 rounded-md ${menu.gap ? "mt-12" : " "}`}>
+              <div className="text-white text-xl">{menu.icon}</div>
+              <Link to={menu.route} onClick={logout}><span className={`${!open && 'hidden'} origin-left duration-200 text-lg text-white`}>{menu.title}</span></Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-      <div className={`${open ? 'ml-44': ''} flex  flex-wrap justify-center`}>
-        <div className="w-80  max-w-sm bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
-          <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
-          <div className="px-5 pb-5 items-end ">
-            <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
-            <p className="text-sm font-normal">This is plat is perfect </p>
-            <div className="flex items-center justify-between">
-              <span className="text-3xl font-bold text-black">$10</span>
-              <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
+      <div className="p-3 px-5 text-2xl font-semibold flex-1 h-screen">
+        <nav className={`${open ? 'ml-72' : 'ml-20'} duration-300 bg-black ml-20 text-white border-gray-200 px-2 rounded-xl sm:px-4 py-2.5 dark:bg-gray-900`}>
+          <div className="container flex flex-wrap items-center justify-between mx-auto">
+            <a href="#" className="flex items-center">
+              <img src="../../../public/assets/logo.png" className="h-6 mr-3 sm:h-9" alt="Marhaba Logo" />
+            </a>
+            <div class="flex items-center md:order-2">
+              <button type="button" class="flex mr-3 text-sm  rounded-full md:mr-0" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                <img className="w-10 h-10 rounded-full bg-white" src="../../../public/assets/profil.png" alt="pPofil photo" />
+              </button>
             </div>
           </div>
-        </div>
-        <div className="w-80 max-w-sm bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
-          <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
-          <div className="px-5 pb-5 items-end ">
-            <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
-            <p className="text-sm font-normal">This is plat is perfect </p>
-            <div className="flex items-center justify-between">
-              <span className="text-3xl font-bold text-black">$10</span>
-              <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
-            </div>
-          </div>
-        </div>
-        <div className="w-80 max-w-sm bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
-          <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
-          <div className="px-5 pb-5 items-end ">
-            <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
-            <p className="text-sm font-normal">This is plat is perfect </p>
-            <div className="flex items-center justify-between">
-              <span className="text-3xl font-bold text-black">$10</span>
-              <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
-            </div>
-          </div>
-        </div>
-        <div className="w-80 max-w-sm bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
-          <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
-          <div className="px-5 pb-5 items-end ">
-            <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
-            <p className="text-sm font-normal">This is plat is perfect </p>
-            <div className="flex items-center justify-between">
-              <span className="text-3xl font-bold text-black">$10</span>
-              <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
-            </div>
-          </div>
-        </div>
-        <div className="w-80 max-w-sm bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
-          <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
-          <div className="px-5 pb-5 items-end ">
-            <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
-            <p className="text-sm font-normal">This is plat is perfect </p>
-            <div className="flex items-center justify-between">
-              <span className="text-3xl font-bold text-black">$10</span>
-              <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
-            </div>
-          </div>
-        </div>
-        <div className="w-80 max-w-sm bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
-          <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
-          <div className="px-5 pb-5 items-end ">
-            <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
-            <p className="text-sm font-normal">This is plat is perfect </p>
-            <div className="flex items-center justify-between">
-              <span className="text-3xl font-bold text-black">$10</span>
-              <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
-            </div>
-          </div>
-        </div>
-          <div className="w-80 max-w-sm bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
+        </nav>
+        
+        <div className={`${open ? 'ml-72' : 'ml-20'} mb-4 duration-200 flex flex-wrap justify-center`}>
+          <div className="w-80  max-w-sm mb-4 bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
             <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
             <div className="px-5 pb-5 items-end ">
               <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
@@ -84,25 +77,92 @@ function Client() {
               </div>
             </div>
           </div>
-        <div className="w-80 max-w-sm bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
-          <img className="p-8 rounded-t-lg" src="../../../../public/assets/plat2.png" alt="product image" />
-          <div className="px-5 pb-5 items-end ">
-            <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
-            <p className="text-sm font-normal">This is plat is perfect </p>
-            <div className="flex items-center justify-between">
-              <span className="text-3xl font-bold text-black">$10</span>
-              <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
+          <div className="w-80 max-w-sm mb-4 bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
+            <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
+            <div className="px-5 pb-5 items-end ">
+              <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
+              <p className="text-sm font-normal">This is plat is perfect </p>
+              <div className="flex items-center justify-between">
+                <span className="text-3xl font-bold text-black">$10</span>
+                <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="w-80 max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 mt-4 drop-shadow-2xl mr-6">
-          <img className="p-8 rounded-t-lg" src="../../../../public/assets/plat3.png" alt="product image" />
-          <div className="px-5 pb-5 items-end ">
-            <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
-            <p className="text-sm font-normal">This is plat is perfect </p>
-            <div className="flex items-center justify-between">
-              <span className="text-3xl font-bold text-black">$10</span>
-              <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
+          <div className="w-80 max-w-sm mb-4 bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
+            <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
+            <div className="px-5 pb-5 items-end ">
+              <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
+              <p className="text-sm font-normal">This is plat is perfect </p>
+              <div className="flex items-center justify-between">
+                <span className="text-3xl font-bold text-black">$10</span>
+                <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-80 max-w-sm mb-4 bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
+            <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
+            <div className="px-5 pb-5 items-end ">
+              <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
+              <p className="text-sm font-normal">This is plat is perfect </p>
+              <div className="flex items-center justify-between">
+                <span className="text-3xl font-bold text-black">$10</span>
+                <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-80 max-w-sm mb-4 bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
+            <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
+            <div className="px-5 pb-5 items-end ">
+              <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
+              <p className="text-sm font-normal">This is plat is perfect </p>
+              <div className="flex items-center justify-between">
+                <span className="text-3xl font-bold text-black">$10</span>
+                <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-80 max-w-sm mb-4 bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
+            <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
+            <div className="px-5 pb-5 items-end ">
+              <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
+              <p className="text-sm font-normal">This is plat is perfect </p>
+              <div className="flex items-center justify-between">
+                <span className="text-3xl font-bold text-black">$10</span>
+                <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-80 max-w-sm mb-4 bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
+            <img className="p-8 rounded-t-lg " src="../../../../public/assets/plat1.png" alt="product image" />
+            <div className="px-5 pb-5 items-end ">
+              <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
+              <p className="text-sm font-normal">This is plat is perfect </p>
+              <div className="flex items-center justify-between">
+                <span className="text-3xl font-bold text-black">$10</span>
+                <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-80 max-w-sm mb-4 bg-white rounded-lg shadow-md mt-4 drop-shadow-2xl mr-6">
+            <img className="p-8 rounded-t-lg" src="../../../../public/assets/plat2.png" alt="product image" />
+            <div className="px-5 pb-5 items-end ">
+              <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
+              <p className="text-sm font-normal">This is plat is perfect </p>
+              <div className="flex items-center justify-between">
+                <span className="text-3xl font-bold text-black">$10</span>
+                <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-80 max-w-sm mb-4 bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 mt-4 drop-shadow-2xl mr-6">
+            <img className="p-8 rounded-t-lg" src="../../../../public/assets/plat3.png" alt="product image" />
+            <div className="px-5 pb-5 items-end ">
+              <h1 className="font-semibold tracking-tight text-black text-4xl">Plat Chicken</h1>
+              <p className="text-sm font-normal">This is plat is perfect </p>
+              <div className="flex items-center justify-between">
+                <span className="text-3xl font-bold text-black">$10</span>
+                <Link to='#' className="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</Link>
+              </div>
             </div>
           </div>
         </div>
--- frontend-vite/src/pages/user/client/settingClient.jsx
+++ frontend-vite/src/pages/user/client/settingClient.jsx
@@ -1,32 +1,97 @@
-import React from "react";
+import { React, useState } from "react"
 import { Link } from "react-router-dom";
+import { AiFillSetting, AiOutlineDashboard, AiOutlineLogout } from 'react-icons/ai'
+import { GiHotMeal } from 'react-icons/gi'
 import Input from "../../../components/Input";
 import Button from "../../../components/Button";
+import axios from "axios";
+
+const baseURLLogout = 'http://localhost:5500/api/auth'
 
 
-function Setting() {
+function SettingClient() {
+
+  function logout() {
+    axios.get(`${baseURLLogout}/logout`)
+      .then((res) => {
+        if (res.data) {
+          localStorage.clear()
+        }
+      })
+      .catch(err =>
+        console.log(err)
+      )
+  }
+
+  const [open, setOpen] = useState(true)
+
+  const MenusClient = [
+    { title: "Dashboad", icon: <AiOutlineDashboard />, route: '/dashboard/client' },
+    { title: "Salade", icon: <GiHotMeal />, gap: true, route: '/' },
+    { title: "Pizza", icon: <GiHotMeal />, route: '/' },
+    { title: "Tacos", icon: <GiHotMeal />, route: '/' },
+    { title: "Sandwich", icon: <GiHotMeal />, route: '/' },
+    { title: "Desserts", icon: <GiHotMeal />, route: '/' },
+    { title: "Setting", icon: <AiFillSetting />, gap: true, route: '/dashboard/client/setting' },
+    { title: "Logout", icon: <AiOutlineLogout />, route: '/login' },
+  ]
+
   return (
     <div>
-      <form className="p-4 pt-9">
-        <div class="relative z-0 mb-6 w-full group">
-          <Input type="text" name="first_name" id="first_name" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="First Name" required />
+      <div className={`${open ? 'w-72' : 'w-20'} fixed top-0 duration-300 px-5 min-h-screen  bg-dark`}>
+        <img src="../../../../public/assets/left-arrow.png" className={`bg-white absolute cursor-pointer rounded-full -right-3 top-9 w-8 border-4 p-1 border-dark ${!open && "rotate-180"}`} onClick={() => setOpen(!open)} />
+        <div className="flex gap-x-4 items-center justify-center ">
+          <img src="../../../../public/assets/logo.png" className={`cursor-pointer w-16`} />
         </div>
-        <div class="relative z-0 mb-6 w-full group">
-          <Input type="text" name="last_name" id="last_name" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="Last Name" required />
-        </div>
-        <div class="relative z-0 mb-6 w-full group">
-          <Input type="email" name="email" id="email" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="Email" required />
-        </div>
-        <div class="relative z-0 mb-6 w-full group">
-          <Input type="text" name="phone" id="phone" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="phone" required />
-        </div>
-        <div class="relative z-0 mb-6 w-full group">
-          <Input type="text" name="city" id="city" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="City" required />
-        </div>
-        <Button type="submit" class="text-white bg-black hover:bg-neutral-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto mt-3 px-9 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" btn="Submit" />
-      </form>
+        <ul className="pt-6">
+          {MenusClient.map((menu, index) => (
+            <li key={index} className={`text-gray-300 text-sm flex w-11 items-center gap-x-4 cursor-pointer p-2 hover:bg-zinc-800 rounded-md ${menu.gap ? "mt-12" : " "}`}>
+              <div className="text-white text-xl">{menu.icon}</div>
+              <Link to={menu.route} onClick={logout}><span className={`${!open && 'hidden'} origin-left duration-200 text-lg text-white`}>{menu.title}</span></Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="p-3 px-5 text-2xl font-semibold flex-1 h-screen">
+        <nav className={`${open ? 'ml-72' : 'ml-20'} duration-300 bg-black ml-20 text-white border-gray-200 px-2 rounded-xl sm:px-4 py-2.5 dark:bg-gray-900`}>
+          <div className="container flex flex-wrap items-center justify-between mx-auto">
+            <a href="#" className="flex items-center">
+              <img src="../../../public/assets/logo.png" className="h-6 mr-3 sm:h-9" alt="Marhaba Logo" />
+            </a>
+            <div class="flex items-center md:order-2">
+              <button type="button" class="flex mr-3 text-sm  rounded-full md:mr-0" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                <img className="w-10 h-10 rounded-full bg-white" src="../../../public/assets/profil.png" alt="pPofil photo" />
+              </button>
+            </div>
+          </div>
+        </nav>
+
+        <form className={`${open ? 'ml-72' : 'ml-20'} duration-300 p-4 pt-9`}>
+          <div class="relative z-0 mb-6 w-full group">
+            <Input type="text" name="first_name" id="first_name" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="First Name" required />
+          </div>
+          <div class="relative z-0 mb-6 w-full group">
+            <Input type="text" name="last_name" id="last_name" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="Last Name" required />
+          </div>
+          <div class="relative z-0 mb-6 w-full group">
+            <Input type="email" name="email" id="email" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="Email" required />
+          </div>
+          <div class="relative z-0 mb-6 w-full group">
+            <Input type="text" name="phone" id="phone" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="phone" required />
+          </div>
+          <div class="relative z-0 mb-6 w-full group">
+            <Input type="text" name="city" id="city" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="City" required />
+          </div>
+          <Button type="submit" class="text-white bg-black hover:bg-neutral-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto mt-3 px-9 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" btn="Submit" />
+        </form>
+      </div>
+
+
     </div>
+
+
   )
 }
 
-export default Setting
+export default SettingClient
