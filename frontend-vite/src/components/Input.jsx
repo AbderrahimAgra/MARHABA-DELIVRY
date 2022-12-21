3fb5fd0034917bb27cc616457c48c546917e61a6
abdenacer <81742528+abdnacer@users.noreply.github.com>
2022-12-21 16:16:29 +0800

    Mar 1 authentification (#44)
    * MAR-7 Create collection in database
    
    * MAR-23 done login and register
    
    * ..
    
    * Cote backend is done
    
    * MAR-25 creation des page front end
    
    * MAR-42 fini la liason back et frontend les 2 pages signup et login
    
    * fix bug
    
    * done
    
    * done
    
    * ...
    
    * ..
    
    * modifier beaucoup de choose le cote front-end
    
    * modifier sur la page category
    
    * modifier sidebar par le role
    
    * modifier sur sidebar des projet en generale
    
    * modifier navbar client
    
    * modifier sur dashboard
    
    Co-authored-by: AbderrahimAgra <93934437+AbderrahimAgra@users.noreply.github.com>

--- frontend-vite/src/components/Input.jsx
+++ frontend-vite/src/components/Input.jsx
@@ -3,8 +3,7 @@ import React from "react";
 function Input(props) {
   return(
     <div>
-      <input checked={props.checked} type={props.type} className={props.class} id={props.id} onChange={props.onChange} placeholder={props.placeholder} name={props.name}  />
-      {/* style={{ backgroundColor: '#1f2937' }} */}
+      <input checked={props.checked} type={props.type} value={props.value} className={props.class} id={props.id} onChange={props.onChange} placeholder={props.placeholder} name={props.name}  />
     </div>
   )
 }
