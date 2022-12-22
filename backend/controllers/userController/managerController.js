ce9d2a177477e1beb9e1f8d5db7f2837730a0b06
elmehdinainia <mehdinainiaa4@gmail.com>
2022-12-22 09:14:53 +0100

    MAR-44 ajouter repas delete repas

--- backend/controllers/userController/managerController.js
+++ backend/controllers/userController/managerController.js
@@ -27,16 +27,13 @@ const managerUser = async (req, res) => {
 
 const addcategory = async (req, res) => {
   const { name } = req.body
-  if (name == '') throw Error("Please Fill The name")
+  if (name == '') res.status(401).send("Please Fill The name")
   const category = await Category.findOne({ name })
-  if (category) throw Error("the category deja already")
+  if (category) res.status(401).send("the category deja already")
   if (!category) {
     const category_create = await Category.create({ name })
     if (category_create) {
-      res.json({
-        name: name,
-        message: "created category"
-      })
+      res.status(200).send("created category")
     }
 
     //!------------------------------------------------autre methode create---------------------------------
@@ -65,9 +62,9 @@ const deletcategory = async (req, res) => {
   const finddeleted = await Category.findOne({ id })
   if (finddeleted) {
     await finddeleted.remove()
-    res.send(`${finddeleted.name} deleted succesfully`)
+    res.status(200).send(`deleted succesfully`)
   }
-  else throw Error('not deleted')
+  else res.status(401).send('not deleted')
 
 }
 
@@ -163,8 +160,15 @@ const addimage = async (req, res) => {
     throw new Error("product is not added");
 
   }
-
 }
+// res.send({
+//   file: req.file.filename,
+//   path: req.path
+// })
+// const path = req.file.path;
+
+       
+
 // jai un probleme file systemenje les resoudrÃ©
 const deletproduct = async (req, res) => {
   const id = req.params.id;
@@ -198,23 +202,9 @@ const GetAllProduct = async (req, res) => {
     throw new Error(error);
   }
 };
+
 const updateproduct = async (req, res) => {
-  // const {id} = req.params
-  // const updateprod = {
-  //   name:req.body.Categoryname, 
-  //   description:req.body.description,
-  //   price:req.body.price,
-  //   category:req.body.category,
-  // }
-  // const isformfield = Object.values(updateprod).every((value)=>{
-  //   if(value){
-  //     return true;
-  //   }
-  //   else {
-  //     return false;
-  //   }
-  // })
-  // const finddata = awiat Meal.findById({_id:id})
+
   const { id } = req.params
   console.log(req.params)
   const UpdatedProduct = {
@@ -243,16 +233,6 @@ const updateproduct = async (req, res) => {
     });
   }
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
--- frontend-vite/src/pages/user/manager/categoryManager.jsx
+++ frontend-vite/src/pages/user/manager/categoryManager.jsx
@@ -6,6 +6,7 @@ import { AiOutlineCloseCircle } from 'react-icons/ai';
 import Button from '../../../components/Button';
 import Input from '../../../components/Input';
 import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
 
 const baseURL = 'http://localhost:5500/api/user/manager'
 const baseURLLogout = 'http://localhost:5500/api/auth'
@@ -13,7 +14,44 @@ const baseURLLogout = 'http://localhost:5500/api/auth'
 function categoryManager() {
 
   const [showModal, setShowModal] = useState(false)
+  const [showModaledit, setShowModaledit] = useState(false)
+
   const [category, setcategory] = useState([])
+  const [name, setName] = useState()
+  const [edit, setEdit] = useState({name: ''})
+
+  const handeName = (e) =>{
+    const valeur = e.target.value
+    setEdit({...edit, [e.target.name]: valeur})
+    console.log(edit)
+  }
+
+  const registerHandler = async () => {
+    const user = {
+      name,
+    }
+    const result = await axios.post(`${baseURL}/addcategory`,user)
+    .then((res) =>{
+      toast.success(res.data)
+      window.location.reload(false)
+
+    }) 
+    .catch ((error)=>{
+      toast.error(error.response.data);
+    })
+  }
+
+  const deleted = async (id) => {
+    await axios.delete(`${baseURL}/deletcategory/${id}`)
+      .then((e) => {
+        toast.success(e.data)
+        window.location.reload(false)
+      })
+      .catch((err) => {
+        toast.error(err.e.data);
+      })
+
+  }
 
   const affichcategory = async () => {
     const datarepas = await axios.get(`${baseURL}/findcategory`)
@@ -57,8 +95,8 @@ function categoryManager() {
                     {cate.name}
                   </td>
                   <td className="py-4 px-6 flex items-center">
-                    <Link to="#" className="text-black text-xl mr-3"><FiEdit /></Link>
-                    <Link to="#" className="text-black text-3xl"><MdDeleteSweep /></Link>
+                    <Link to='' className="text-black text-xl mr-3" onClick={() => {setShowModaledit(true); setEdit(cate)}}><FiEdit /></Link>
+                    <Link to="#" onClick={deleted} className="text-black text-3xl"><MdDeleteSweep /></Link>
                   </td>
                 </tr>
               ))}
@@ -88,12 +126,13 @@ function categoryManager() {
                   <form className="my-4 text-slate-500 text-lg leading-relaxed">
                     <div className="flex flex-col">
                       <div className="mb-2">
-                        <Input type="text" name="name" id="name" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="Name Category" required />
+                        <Input type="text" value={name} onChange={handeName} name="name" id="name" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="Name Category" required />
                       </div>
                     </div>
                     <div className="flex justify-center p-6 border-t border-solid border-slate-200 rounded-b">
                       <Button type='button' class='text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg w-full text-sm px-2 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800' onClick={() => setShowModal(false)} btn='Close' />
-                      <Button type='button' class='text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg w-full text-sm px-1.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800' btn='Create Repas' />
+                      {/* <Button type='button' onClick={registerHandler} className='text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg w-full text-sm px-1.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800' btn='Create Repas' /> */}
+                      <button type='button' onClick={registerHandler} className='text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg w-full text-sm px-1.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800'>valider</button>
                     </div>
                   </form>
                 </div>
@@ -104,6 +143,46 @@ function categoryManager() {
         </>
       ) : null
       }
+        {showModaledit ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t ">
+                  <h3 className="text-3xl font-semibold">
+                    Ajouter un Repas
+                  </h3>
+                  <button className="p-1 bg-transparent border-0 text-gray-300 opacity-1 float-right text-3xl leading-none font-semibold outline-none focus:outline-none ml-8" onClick={() => setShowModaledit(false)} >
+                    <span className=" text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      <AiOutlineCloseCircle />
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <form className="my-4 text-slate-500 text-lg leading-relaxed">
+                    <div className="flex flex-col">
+                      <div className="mb-2">
+                        <Input type="text" value={edit.name} onChange={handeName} name="name" id="name" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer" placeholder="Name Category" required />
+                      </div>
+                    </div>
+                    <div className="flex justify-center p-6 border-t border-solid border-slate-200 rounded-b">
+                      <Button type='button' class='text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg w-full text-sm px-2 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800' onclick={() => setShowModaledit(false)} btn='Close' />
+                      {/* <Button type='button' onClick={registerHandler} className='text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg w-full text-sm px-1.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800' btn='Create Repas' /> */}
+                      <button type='button' onClick={registerHandler} className='text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg w-full text-sm px-1.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800'>valider</button>
+                    </div>
+                  </form>
+                </div>
+                {/*footer*/}
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null
+      }
+      <ToastContainer/>
     </div>
   )
 }
