c88ac89f4ed2f87a40458ef15f10a056f6db62ce
EL Mehdi Nainia <93974407+elmehdinainia@users.noreply.github.com>
2022-12-26 11:14:21 +0100

    Mar 48 frontend consomation mehdi (#53)
    * Fini mon tache de cote backend
    
    * ..
    
    * jai resoudre probleme de model
    
    * ...
    
    * ..
    
    * ..
    
    * ..
    
    * .
    
    * ..
    
    * ..
    
    * MAR-44 ajouter repas delete repas
    
    * bann des utilisateur
    
    * mar-50 update repas
    
    * realiser un profile de user

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -21,7 +21,8 @@ import LivreursManager from './pages/user/manager/LivreursManager'
 import ClientsManager from './pages/user/manager/clientsManager'
 import SettingManager from './pages/user/manager/settingManager'
 import 'react-toastify/dist/ReactToastify.css';
-// import DashbordLivreur from './pages/user/livreur/dashboardLivreur'
+import DashbordLivreur from './pages/user/livreur/dashboardLivreur'
+
 
 // window.addEventListener("storage", () => {
 //   axios
